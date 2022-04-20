99517efab842aaaca7d6eac9ee1dedfbc17c631b
Harsh Krishnani <harshkrishnani2409@gmail.com>
2022-04-20 10:20:59 +0530

    files added

---
+++ datadis.js
@@ -0,0 +1,12 @@
+document.getElementById("fnme").innerHTML = localStorage.getItem("firstName");
+document.getElementById("lnme").innerHTML = localStorage.getItem("lastName");
+document.getElementById("eml").innerHTML = localStorage.getItem("emaill");
+document.getElementById("ag").innerHTML = localStorage.getItem("agee");
+
+document.getElementById("dropdown1").innerHTML = localStorage.getItem("custommond");
+document.getElementById("dropdown2").innerHTML = localStorage.getItem("customtues");
+document.getElementById("dropdown3").innerHTML = localStorage.getItem("customwednes");
+document.getElementById("dropdown4").innerHTML = localStorage.getItem("customthurs");
+document.getElementById("dropdown5").innerHTML = localStorage.getItem("customfrid");
+document.getElementById("dropdown6").innerHTML = localStorage.getItem("customsatur");
+document.getElementById("dropdown7").innerHTML = localStorage.getItem("customsund");
